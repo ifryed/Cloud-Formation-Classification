ae5f5ae3d0de0610ef89ef1193743a6ceca17a58
Shai <ifryed@gmail.com>
2019-12-23 11:21:41 +0200

    Comments on the models

--- Perceptron.py
+++ Perceptron.py
@@ -3,6 +3,10 @@ import numpy as np
 
 
 class Perceptron:
+    """
+    A model of a Single-layer network
+    """
+
     def __init__(self, input_num: int, class_num: int):
         self.input_num = input_num
         self.class_num = class_num
@@ -10,8 +14,9 @@ class Perceptron:
         self.weights, self.biases = self.getWeights()
 
     def getWeights(self) -> (dict, dict):
-        # Store layers weight & bias
-
+        """
+        Store layers weight & bias
+        """
         weights = {
             'out': tf.Variable(tf.random.truncated_normal([self.input_num, self.class_num], stddev=0.1))
         }
@@ -23,9 +28,9 @@ class Perceptron:
         tf.add_to_collection(tf.GraphKeys.REGULARIZATION_LOSSES, biases['out'])
         return weights, biases
 
-    # Define the neural network
     def getModel(self, x: np.ndarray):
-        # Output fully connected layer with a neuron for each class
+        """
+        Output fully connected layer with a neuron for each class
+        """
         out_layer = tf.add(tf.matmul(x, self.weights['out']), self.biases['out'])
         return out_layer
-
--- SimpleAnn.py
+++ SimpleAnn.py
@@ -3,6 +3,9 @@ import numpy as np
 
 
 class SimpleAnn:
+    """
+    A model of a simple Multi-layer network
+    """
 
     def __init__(self, hidden_lst: list, input_num: int, class_num: int):
         self.hidden_layers = list(hidden_lst)
@@ -12,7 +15,9 @@ class SimpleAnn:
         self.weights, self.biases = self.getWeights()
 
     def getWeights(self) -> (dict, dict):
-        # Store layers weight & bias
+        """
+        Store layers weight & bias
+        """
         weights = dict()
         biases = dict()
         last_output = self.input_num
@@ -31,18 +36,21 @@ class SimpleAnn:
         tf.add_to_collection(tf.GraphKeys.REGULARIZATION_LOSSES, biases['out'])
         return weights, biases
 
-    # Define the neural network
-    def getModel(self, x: np.ndarray):
-        global num_classes
-        # Output fully connected layer with a neuron for each class
-        layers_keys = list(self.weights.keys())
-        Ls = [tf.matmul(x, self.weights[layers_keys[0]]) + self.biases[layers_keys[0]]]
-        relus = [tf.nn.relu(Ls[-1])]
-        for key in layers_keys[1:-1]:
-            newL = tf.add(tf.matmul(relus[-1], self.weights[key]), self.biases[key])
-            Ls.append(newL)
-            relus.append(tf.nn.sigmoid(newL))
 
-        out_layer = tf.add(tf.matmul(relus[-1], self.weights['out']), self.biases['out'])
+def getModel(self, x: np.ndarray):
+    """
+    Build the network model, based on the input and the weights
+    """
+    global num_classes
+    # Output fully connected layer with a neuron for each class
+    layers_keys = list(self.weights.keys())
+    Ls = [tf.matmul(x, self.weights[layers_keys[0]]) + self.biases[layers_keys[0]]]
+    relus = [tf.nn.relu(Ls[-1])]
+    for key in layers_keys[1:-1]:
+        newL = tf.add(tf.matmul(relus[-1], self.weights[key]), self.biases[key])
+        Ls.append(newL)
+        relus.append(tf.nn.sigmoid(newL))
 
-        return out_layer
+    out_layer = tf.add(tf.matmul(relus[-1], self.weights['out']), self.biases['out'])
+
+    return out_layer
